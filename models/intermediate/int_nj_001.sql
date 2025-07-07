158dae504cdb169ec67b7701ae64d5a3ee9339c8
Bianca Niemann <payrollsollutions@gmail.com>
2025-07-07 13:03:25 +0000

    int models created

---
+++ models/intermediate/int_nj_001.sql
@@ -0,0 +1,10 @@
+select 
+    month,
+    movie_id,
+    location_id,
+    sum(tickets_sold) as tickets_sold,
+    sum(ticket_price) as ticket_price,
+    sum(revenue) as revenue
+from {{ ref('stg_nj_001') }}
+group by month, movie_id, location_id
+order by month
