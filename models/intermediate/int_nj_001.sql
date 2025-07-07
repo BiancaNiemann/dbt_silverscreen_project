bf5b6c6d06d44209e43f0bbed0b48479bc09c83d
Bianca Niemann <payrollsollutions@gmail.com>
2025-07-07 16:04:38 +0000

    remove sum(price) from int_nj tables and created final table

--- models/intermediate/int_nj_001.sql
+++ models/intermediate/int_nj_001.sql
@@ -3,7 +3,6 @@ select
     movie_id,
     location_id,
     sum(tickets_sold) as tickets_sold,
-    sum(ticket_price) as ticket_price,
     sum(revenue) as revenue
 from {{ ref('stg_nj_001') }}
 group by month, movie_id, location_id
